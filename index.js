9a1c41785c8f6c22328e5ba65af8e976091c74c9
Eddie Roosenmaallen <silvermoon82@gmail.com>
2019-05-28 09:18:43 -0400

    wip

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+/** @file       index.js
+ *  @author     Eddie Roosenmaallen <silvermoon82@gmail.com>
+ *  @date       May 2019
+ *
+ *  This file implements the main UI of Faraday Dam
+ */
+
